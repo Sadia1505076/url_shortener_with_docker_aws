8068488023c1b92678d0afd856be47d47a556f0a
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2023-02-06 02:20:21 +0600

    dummy export

--- pages/[url].tsx
+++ pages/[url].tsx
@@ -2,8 +2,11 @@ import prisma from 'lib/prisma';
 import moment from 'moment';
 import { GetServerSideProps } from "next";
 
+export default function dummy() {
+  // dummy default export for the redirection to work
+}
+
 export const getServerSideProps: GetServerSideProps = async ({query}) => {
-  console.log("inside server side props:",query.url);
   let short_url:   string | null = query.url?.toString() || null;
   let destination: string        = "/404";
   if (short_url != null && short_url != undefined) {
@@ -18,6 +21,6 @@ export const getServerSideProps: GetServerSideProps = async ({query}) => {
     redirect: {
       destination: destination,
       permanent:   false,
-    },
+    }
   }
 }
